db5712f4a86ff07970a67875711a9cbf49c02b83
jerepsky <jj@uniscale.com>
2023-03-14 14:01:14 +0200

    DISABLE CORS

--- hackaton-super-retro/Program.cs
+++ hackaton-super-retro/Program.cs
@@ -19,4 +19,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    //.WithOrigins("https://localhost:44351")); // Allow only this origin can also have multiple origins separated with comma
+    );
+
 app.Run();
